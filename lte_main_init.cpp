3c8e96fcc0c22493e9c395a277de78de5b22bc87
prashanth-trackbit <prashanth@trackbit.in>
2018-05-15 08:23:29 +0530

    srv_module_checkin

--- lte_main_init.cpp
+++ lte_main_init.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 //#include "../rrc/inc/rrc.h
 #include "rrc/inc/rrc.h"
+#include "srv/inc/srv.h"
 
 using namespace std;
 
@@ -8,9 +9,16 @@ int main()
 {
   printf("starting project");
   /**project starts to compile from here**/
+#ifdef STUBS_FRAMEWORK
 
+printf("stubs running");
+
+#endif // STUBS_FRAMEWORK
+
+MSG_O(LTE_INIT);
 
 
   rrc_init();
   /**rrc_init function is called which is defined in RRC layer**/
 }
+
---
+++ srv/inc/srv.h
@@ -0,0 +1,11 @@
+#ifndef SRV_H_INCLUDED
+#define SRV_H_INCLUDED
+
+
+
+#define LTE_INIT "LTE_INIT"
+#define MSG_O(a) printf("%s",a)
+#define MSG_1(b) printf("%s %s",a,b)
+
+
+#endif // SRV_H_INCLUDED
